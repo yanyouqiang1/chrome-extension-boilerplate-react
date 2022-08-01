878c30f6120143a4d5ee56c0400efe64ecdab4b9
yyq <374580618@qq.com>
2022-08-01 20:48:51 +0800

    调整Pop文件

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,71 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
-import './Popup.css';
+import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
+import {render} from "react-dom";
 
-const Popup = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
-  );
+import {AppstoreOutlined, MailOutlined, SettingOutlined} from '@ant-design/icons';
+import {Menu} from 'antd';
+import React, {useState} from 'react';
+
+function getItem(label, key, icon, children, type) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    };
+}
+
+const items = [
+    getItem('常用功能', 'sub1', <MailOutlined/>, [
+        getItem('YAML/JSON工具', 'yml.html'),
+        getItem('Option 2', '2'),
+        getItem('Option 3', '3'),
+        getItem('Option 4', '4'),
+    ]),
+    getItem('Navigation Two', 'sub2', <AppstoreOutlined/>, [
+        getItem('Option 5', '5'),
+        getItem('Option 6', '6'),
+        getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+    ]),
+    getItem('Navigation Three', 'sub4', <SettingOutlined/>, [
+        getItem('Option 9', '9'),
+        getItem('Option 10', '10'),
+        getItem('Option 11', '11'),
+        getItem('Option 12', '12'),
+    ]),
+]; // submenu keys of first level
+
+const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+
+const App = () => {
+    const [openKeys, setOpenKeys] = useState(['sub1']);
+
+    const onOpenChange = (keys) => {
+        const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+
+        if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            setOpenKeys(keys);
+        } else {
+            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+        }
+    };
+    const onSelect = (item, key, keyPath, selectedKeys, domEvent) => {
+        const w = window.open('about:blank');
+        w.location.href = item.key
+    }
+    return (
+        <Menu
+            mode="inline"
+            openKeys={openKeys}
+            onSelect={onSelect}
+            onOpenChange={onOpenChange}
+            style={{
+                width: 256,
+            }}
+            items={items}
+        />
+    );
 };
 
-export default Popup;
+render(<App/>, window.document.querySelector('#app-container'));
--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ var options = {
     entry: {
         newtab: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.jsx'),
         options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
-        popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
+        popup: path.join(__dirname, 'src', 'pages', 'Popup', 'Popup.jsx'),
         background: path.join(__dirname, 'src', 'pages', 'Background', 'index.js'),
         contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.js'),
         tabs: path.join(__dirname, 'src', 'pages', 'tabs', 'tabs.tsx'),
