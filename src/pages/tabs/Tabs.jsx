96b01c5c8cb935548f0d62a68822f1179d60bf91
yanyouqiang <374580618@qq.com>
2022-08-02 19:37:21 +0800

    全新版本tab

--- src/pages/tabs/Tabs.jsx
+++ src/pages/tabs/Tabs.jsx
@@ -6,107 +6,89 @@ import React, {useEffect, useState} from 'react';
 import Search from "antd/es/input/Search";
 import NewModel from "./NewModel";
 import {getStorage_record_key} from "./ChromeCommon";
+import TextArea from "antd/es/input/TextArea";
 
-const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a>{text}</a>,
-    },
-    {
-        title: '地址',
-        dataIndex: 'address',
-        key: 'address',
-    },
-    {
-        title: '备注',
-        dataIndex: 'remark',
-        key: 'remark',
-    },
-    {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (_, {tags}) => (
-            <>
-                {tags.map((tag) => {
-                    let color = tag.length > 5 ? 'geekblue' : 'green';
 
-                    if (tag === 'loser') {
-                        color = 'volcano';
-                    }
 
-                    return (
-                        <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </>
-        ),
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <a>Invite {record.name}</a>
-                <a>Delete</a>
-            </Space>
-        ),
-    },
-];
-const data = [
-    {
-        key: '1',
-        name: 'John Brown',
-        address: 32,
-        remark: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
-    },
-    {
-        key: '1',
-        name: 'John Brown',
-        address: 32,
-        remark: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
-    },
-    {
-        key: '1',
-        name: 'John Brown',
-        address: 32,
-        remark: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
-    },
-
-];
-
-const onSearch=()=>{
+const onSearch = () => {
 
 }
 
 
-
 const TAB = () => {
-    const [datasource,setDatasource] = useState([])
+    const [datasource, setDatasource] = useState([])
 
-
-    useEffect(()=>{
-        getStorage_record_key(result=>{
-            console.log("tabsRecord: "+result)
-            if (result){
-                setDatasource(result)
-            }else{
-                setDatasource(data)
-            }
+    useEffect(() => {
+        getStorage_record_key(result => {
+            console.log("tabsRecord: " + result)
+            result = result ? result : []
+            setDatasource(result)
         })
-    },[])
+    }, [])
 
+    const deleteRecord = (record) => {
+        console.log('delete')
+        let newSource = datasource.filter(item=>item.title!=record.title)
+        setDatasource(newSource)
+    }
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: '地址',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: '备注',
+            dataIndex: 'remark',
+            key: 'remark',
+            // render: (_, {remark}) => (
+            //     <TextArea value={remark} onChange={}></TextArea>
+            // )
+        },
+        {
+            title: 'Tags',
+            key: 'tags',
+            dataIndex: 'tags',
+            render: (_, {tags}) => (
+                <>
+                    {tags.map((tag) => {
+                        let color = tag.length > 5 ? 'geekblue' : 'green';
+
+                        if (tag === 'loser') {
+                            color = 'volcano';
+                        }
+
+                        return (
+                            <Tag color={color} key={tag}>
+                                {tag.toUpperCase()}
+                            </Tag>
+                        );
+                    })}
+                </>
+            ),
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a>Edit</a>
+                    <a onClick={()=>deleteRecord(record)}>Delete</a>
+                </Space>
+            ),
+        },
+    ];
 
     return (
         <>
-            <NewModel />
+            <NewModel/>
             <Search
                 placeholder="input search text"
                 allowClear
@@ -115,7 +97,7 @@ const TAB = () => {
                 onSearch={onSearch}
             />
             <br/>
-            <Table columns={columns} dataSource={datasource} />
+            <Table columns={columns} dataSource={datasource}/>
         </>
     )
 }
