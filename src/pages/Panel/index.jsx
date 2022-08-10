8edeead304c2847fe8e1956966c0bd568bf3a328
yyq <374580618@qq.com>
2022-08-10 22:07:58 +0800

    一个可以执行的panel

--- src/pages/Panel/index.jsx
+++ src/pages/Panel/index.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, {useEffect, useState} from 'react';
+import {render} from 'react-dom';
 
 import Panel from './Panel';
 import './index.css';
+import {Button} from "antd";
 
-render(<Panel />, window.document.querySelector('#app-container'));
+const allData = {};
+
+const handleData = (tempRequest) => {
+    const {_resourceType, response, request} = tempRequest;
+    const {url} = request;
+    const {content} = response;
+    const [type, mineType] = content.mimeType.split("/"); // image/gif
+    const tempVal = {
+        type,
+        url,
+        mineType,
+        tempRequest,
+    };
+    allData[_resourceType]
+        ? allData[_resourceType].push(tempVal)
+        : (allData[_resourceType] = [tempVal]);
+};
+
+chrome.devtools.network.onRequestFinished.addListener(function (request) {
+    handleData(request);
+    // chrome.runtime.sendMessage({
+    //     source: "devtools",
+    //     data: allData,
+    // });
+});
+
+const MyPanel = () => {
+    const [result, setResult] = useState('');
+
+    function log(str) {
+        chrome.devtools.inspectedWindow.eval(
+            `console.log(${str})`
+        );
+    }
+    // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    //     setResult(JSON.stringify(message.data));
+    // })
+    useEffect(() => {
+        setResult(JSON.stringify(allData));
+    }, [])
+
+    const test = () => {
+        setResult(JSON.stringify(allData));
+        // log("send")
+        // chrome.runtime.sendMessage({
+        //     source: "refreash",
+        // });
+    }
+    return (
+        <>
+            <Button onClick={test}>刷新</Button>
+            <textarea value={result}></textarea>
+        </>
+
+    )
+}
+
+
+render(<MyPanel/>, window.document.querySelector('#app-container'));
 
 if (module.hot) module.hot.accept();
