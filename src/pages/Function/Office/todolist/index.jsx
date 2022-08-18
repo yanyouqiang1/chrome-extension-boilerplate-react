aa1e72fa726cea404cc16a31e575a717689c4d9e
yyq <374580618@qq.com>
2022-08-18 11:53:08 +0800

    todo

--- src/pages/Function/Office/todolist/index.jsx
+++ src/pages/Function/Office/todolist/index.jsx
@@ -1,44 +1,124 @@
 import { render } from "react-dom";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "antd/dist/antd.css";
-import { Checkbox, Col, List, Row } from "antd";
+import { Button, Card, Checkbox, Col, List, Row, Typography } from "antd";
 import dayjs from "dayjs";
-import { ExclamationCircleOutlined } from "@ant-design/icons";
+import { CheckCircleOutlined, ClockCircleOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
+import { getStorage_todolist, setStorage_todolist } from "../../chromeCommon";
+import randomstring from "rdm-str";
 
+const STATUS_FINISH = "finish";
+const STATUS_NEW = "new";
 
 const TodoList = () => {
   const initData = [
     {
-      content:"todotodododo",
+      key: "aaaaaa",
+      content: "todotodododo",
+      status: "finish",
       createTime: dayjs().format("YYYY-MM-DD h:mm:ss"),
-      finishTime: ''
+      finishTime: ""
     }
   ];
+  const [datasource, setDataSource] = useState([]);
+  const [newDatasource, setNewDataSource] = useState([]);
+  const [finishDatasource, setFinishDatasource] = useState([]);
 
+  const [inputText, setInputText] = useState("");
+
+  useEffect(() => {
+    getStorage_todolist(datas => {
+      datas = datas ? datas : [];
+      debugger
+      setNewDataSource(datas.filter(it => it.status == STATUS_NEW));
+      setFinishDatasource(datas.filter(it => it.status == STATUS_FINISH));
+
+    });
+  }, [datasource]);
+  const textChange = (e) => {
+    setInputText(e.target.value);
+  };
+  const newRecord = () => {
+    const record =
+      {
+        key: randomstring(7),
+        content: inputText,
+        status: "new",
+        createTime: dayjs().format("YYYY-MM-DD h:mm:ss"),
+        finishTime: ""
+      };
+    let assign = Object.assign([], datasource);
+    assign.unshift(record);
+
+    saveAndFresh(assign);
+  };
+  const deleteRecord = (item) => {
+    if (!confirm("确认删除吗？")) {
+      return;
+    }
+
+    let assign = Object.assign([], datasource);
+    assign.forEach((it, index) => {
+      if (it.key == item.key) {
+        assign.splice(index, 1);
+      }
+    });
+    saveAndFresh(assign);
+  };
+  const finishRecord = (item) => {
+    let assign = Object.assign([], datasource);
+    assign.forEach((it, index) => {
+      if (it.key == item.key) {
+        assign[index].status = STATUS_FINISH;
+      }
+    });
+    saveAndFresh(assign);
+  };
+  const saveAndFresh = (datasource) => {
+    setInputText("");
+    setStorage_todolist(datasource);
+    setDataSource(datasource);
+  };
 
   return (
     <Row>
       <Col span={10}>
-
         <List
+          header={<div><ClockCircleOutlined style={{ color: "red" }} /> TODO</div>}
+          footer={<div>新建：<input value={inputText} onChange={textChange} /> <Button onClick={newRecord}>确认</Button>
+          </div>}
           itemLayout="horizontal"
-          dataSource={initData}
+          dataSource={newDatasource}
           renderItem={(item) => (
             <List.Item>
-              <Checkbox>Checkbox</Checkbox>
-              <ExclamationCircleOutlined />
-              <List.Item.Meta
-                title={<a href="https://ant.design">{item.content}</a>}
-                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-              />
+              <div>
+                <Typography.Text mark>[ITEM]</Typography.Text>
+                {item.content}
+                <Button onClick={() => finishRecord(item)}>done</Button>
+                <Button onClick={() => deleteRecord(item)}>delete</Button>
+              </div>
             </List.Item>
           )}
         />
 
-
       </Col>
       <Col span={10}>
-
+        <List
+          header={<div><CheckCircleOutlined style={{ color: "green" }} /> FINISH</div>}
+          footer={<></>}
+          itemLayout="horizontal"
+          dataSource={finishDatasource}
+          renderItem={(item) => (
+            <List.Item>
+              <div>
+                <Typography.Text mark>[ITEM]</Typography.Text>
+                {item.content}
+                <Button onClick={() => finishRecord(item)}>done</Button>
+                <Button onClick={() => deleteRecord(item)}>delete</Button>
+              </div>
+            </List.Item>
+          )}
+        />
       </Col>
     </Row>
   );
--- src/pages/Function/chromeCommon.js
+++ src/pages/Function/chromeCommon.js
@@ -1,132 +1,145 @@
 //获取当前窗口下的tabs，除去自己
 export async function getCurrentTabsNoActive() {
-    let query = {
-        windowId: chrome.windows.WINDOW_ID_CURRENT,
-        active: false
-    }
-    let tabs = await chrome.tabs.query(query)
-    return tabs
-}
-//获取当前tab
-export async function getCurrentTab() {
-    let query = {
-        windowId: chrome.windows.WINDOW_ID_CURRENT,
-        active: true
-    }
-    let tabs = await chrome.tabs.query(query)
-    return tabs
+  let query = {
+    windowId: chrome.windows.WINDOW_ID_CURRENT,
+    active: false
+  };
+  let tabs = await chrome.tabs.query(query);
+  return tabs;
 }
 
+//获取当前tab
+export async function getCurrentTab() {
+  let query = {
+    windowId: chrome.windows.WINDOW_ID_CURRENT,
+    active: true
+  };
+  let tabs = await chrome.tabs.query(query);
+  return tabs;
+}
 
 
 export function createUrls(urls) {
-    for (const url of urls) {
-        chrome.tabs.create({
-            url: url
-        });
-    }
+  for (const url of urls) {
+    chrome.tabs.create({
+      url: url
+    });
+  }
 }
 
 //######获取所有的缓存
 export function getAllStorageData() {
-    // Immediately return a promise and start asynchronous work
-    return new Promise((resolve, reject) => {
-        // Asynchronously fetch all data from storage.sync.
-        chrome.storage.local.get(null, (items) => {
-            // Pass any observed errors down the promise chain.
-            if (chrome.runtime.lastError) {
-                return reject(chrome.runtime.lastError);
-            }
-            // Pass the data retrieved from storage down the promise chain.
-            resolve(items);
-        });
+  // Immediately return a promise and start asynchronous work
+  return new Promise((resolve, reject) => {
+    // Asynchronously fetch all data from storage.sync.
+    chrome.storage.local.get(null, (items) => {
+      // Pass any observed errors down the promise chain.
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError);
+      }
+      // Pass the data retrieved from storage down the promise chain.
+      resolve(items);
     });
+  });
 }
 
 export function updateAllStorageData(object) {
-    if (object) {
-        chrome.storage.local.set(object)
-    }
+  if (object) {
+    chrome.storage.local.set(object);
+  }
 
 }
 
 
 //######## tabs
-const newTabs_tags_key = 'newTabs_tags_key'
+const newTabs_tags_key = "newTabs_tags_key";
 
 export function getStorage_newTabs_tags(callback) {
-    chrome.storage.local.get([newTabs_tags_key], result => {
-        callback(result.newTabs_tags_key)
-    })
+  chrome.storage.local.get([newTabs_tags_key], result => {
+    callback(result.newTabs_tags_key);
+  });
 }
 
 export function setStorage_newTabs_tags(value) {
-    chrome.storage.local.set({newTabs_tags_key: value})
+  chrome.storage.local.set({ newTabs_tags_key: value });
 }
 
 //######## record
-const newTabs_record_key = 'newTabs_record_key'
+const newTabs_record_key = "newTabs_record_key";
 
 export function getStorage_record_key(callback) {
-    chrome.storage.local.get([newTabs_record_key], result => {
-        callback(result.newTabs_record_key)
-    })
+  chrome.storage.local.get([newTabs_record_key], result => {
+    callback(result.newTabs_record_key);
+  });
 }
 
 export function setStorage_record_key(value) {
-    chrome.storage.local.set({newTabs_record_key: value})
+  chrome.storage.local.set({ newTabs_record_key: value });
 }
 
 //######## scenario
-const scenario_key = 'scenario_key'
+const scenario_key = "scenario_key";
 
 export function getStorage_scenario(callback) {
-    chrome.storage.local.get([scenario_key], result => {
-        callback(result.scenario_key)
-    })
+  chrome.storage.local.get([scenario_key], result => {
+    callback(result.scenario_key);
+  });
 }
 
 export function setStorage_scenario(value) {
-    chrome.storage.local.set({scenario_key: value})
+  chrome.storage.local.set({ scenario_key: value });
 }
 
 //######## textRecord
-const textRecord_key = 'textRecord_key'
+const textRecord_key = "textRecord_key";
 
 export function getStorage_textRecord(callback) {
-    chrome.storage.local.get([textRecord_key], result => {
-        callback(result.textRecord_key)
-    })
+  chrome.storage.local.get([textRecord_key], result => {
+    callback(result.textRecord_key);
+  });
 }
 
 export function setStorage_textRecord(value) {
-    chrome.storage.local.set({textRecord_key: value})
+  chrome.storage.local.set({ textRecord_key: value });
 }
 
 
 //######## textRecord
-const fragement_key = 'fragement_key'
+const fragement_key = "fragement_key";
 
 export function getStorage_fragement(callback) {
-    chrome.storage.local.get([fragement_key], result => {
-        callback(result.fragement_key)
-    })
+  chrome.storage.local.get([fragement_key], result => {
+    callback(result.fragement_key);
+  });
 }
 
 export function setStorage_fragement(value) {
-    chrome.storage.local.set({fragement_key: value})
+  chrome.storage.local.set({ fragement_key: value });
 }
 
 
 //######## fetchList
-const fetch_key = 'fetch_key'
+const fetch_key = "fetch_key";
 
 export function getStorage_fetch(callback) {
-    chrome.storage.local.get([fetch_key], result => {
-        callback(result.fetch_key)
-    })
+  chrome.storage.local.get([fetch_key], result => {
+    callback(result.fetch_key);
+  });
 }
 
 export function setStorage_fetch(value) {
-    chrome.storage.local.set({fetch_key: value})
+  chrome.storage.local.set({ fetch_key: value });
+}
+
+//######## todolist
+const todolist_key = "todolist_key";
+
+export function getStorage_todolist(callback) {
+  chrome.storage.local.get([todolist_key], result => {
+    callback(result.todolist_key);
+  });
+}
+
+export function setStorage_todolist(value) {
+  chrome.storage.local.set({ todolist_key: value });
 }
