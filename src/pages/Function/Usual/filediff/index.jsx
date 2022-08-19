02157603f377392f1d80636bf245dc911d82935f
yyq <374580618@qq.com>
2022-08-19 22:48:08 +0800

    diff差异优化

--- src/pages/Function/Usual/filediff/index.jsx
+++ src/pages/Function/Usual/filediff/index.jsx
@@ -1,76 +1,126 @@
-import {render} from "react-dom";
-import React, {useState} from "react";
-import {Button, Col, Row} from "antd";
-import 'antd/dist/antd.css';
-import {diffChars, diffLines} from "diff";
+import { render } from "react-dom";
+import React, { useState } from "react";
+import { Button, Card, Col, Row, Tooltip } from "antd";
+import "antd/dist/antd.css";
+import { diffChars, diffJson, diffLines, diffSentences } from "diff";
+import { QuestionCircleOutlined } from "@ant-design/icons";
+
 
 const Filediff = () => {
-    const [textLeft, setTextLeft] = useState('')
-    const [textRight, setTextRight] = useState('')
-    const [textDiff, setTextDiff] = useState('')
 
 
-    const leftChange = (e) => {
-        setTextLeft(e.target.value)
-    }
-    const rightChange = (e) => {
-        setTextRight(e.target.value)
-    }
+  const compareChars = () => {
+    const { left, right } = readCompareText();
+
+    let diffs = diffChars(left, right);
+
+    showResult(diffs);
+  };
+
+  const compareLine = () => {
+    const { left, right } = readCompareText();
+    let diffs = diffLines(left, right);
+    showResult(diffs);
+  };
+
+  //展示结果
+  function showResult(diffs) {
+    const fragment = document.createDocumentFragment();
+    diffs.forEach((part) => {
+      // green for additions, red for deletions
+      // grey for common parts
+      const color = part.added ? "green" :
+        part.removed ? "red" : "grey";
+      const span = document.createElement("span");
+      span.style.color = color;
+      span.appendChild(document
+        .createTextNode(part.value));
+      fragment.appendChild(span);
+    });
+
+    const display = document.getElementById("display");
+    display.innerHTML = "";
+    display.appendChild(fragment);
+  }
 
 
-    const compareChars = () => {
-        let diffs = diffChars(textLeft, textRight);
+  //读取文本
+  function readCompareText() {
+    let leftDom = document.getElementById("textLeft");
+    let rightDom = document.getElementById("textRight");
 
-        let diffContent = diffs.map(part => {
-            let color = part.added ? 'green' :
-                part.removed ? 'red' : 'grey';
-            // return part.value.includes('\n') ?
-            //     (<>
-            //         <span style={{color: color}}>{part.value.replace('\n', '')}</span><br/>
-            //     </>)
-            //     : (
-            //         <span style={{color: color}}>{part.value}</span>
-            //     )
-            return (<font style={{color: color}}>{part.value}</font>)
-        });
-        setTextDiff(diffContent)
 
-    }
-    const compareLine = () => {
-        let diffs = diffLines(textLeft, textRight);
-        let diffContent = diffs.map(part => {
-            let color = part.added ? 'green' :
-                part.removed ? 'red' : 'grey';
-            return (
-                <div style={{color: color}}>{part.value}</div>
-            )
-        });
-        setTextDiff(diffContent)
-    }
+    const left = leftDom.innerText;
+    const right = rightDom.innerText;
 
-    return (
+    //去掉格式
+    leftDom.innerText = left;
+    rightDom.innerText = right;
+    return { left, right };
+  }
+
+  return (
+    <>
+      <Card title={
         <>
-            <Row gutter={24}>
-                <Col span={24}>
-                    <Button onClick={compareChars}>compareChars</Button>
-                    <Button onClick={compareLine}>compareLine</Button>
-                    {/*<Button onClick={compare}>compareChars</Button>*/}
-                </Col>
-            </Row>
-            <Row gutter={24}>
-                <Col span={8}>
-                    <textarea style={{width: '100%'}} value={textLeft} onChange={leftChange} rows={30}></textarea>
-                </Col>
-                <Col span={8}>
-                    <textarea style={{width: '100%'}} value={textRight} onChange={rightChange} rows={30}></textarea>
-                </Col>
-                <Col span={8}>
-                    <textarea rows={30} value={textDiff}></textarea>
-                </Col>
-            </Row>
+          <Button onClick={compareChars} type={"primary"}>compareChars</Button>
+          <Button onClick={compareLine} type={"primary"} style={{ marginLeft: 10 }}>compareLine</Button>
+
         </>
+      }
+            extra={
+              <>
+                <Button onClick={compareChars} type="dashed" danger>clear</Button>
+                <Tooltip title={
+                  <>
+                    <p>左边 对比 右边 </p>
+                    <p><font style={{ color: "green" }}>绿色</font>表示 左边 缺少</p>
+                    <p><font style={{ color: "red" }}>红色</font>表示 左边 多余</p>
+                    <p><font style={{ color: "gray" }}>灰色</font>表示两边相同</p>
+                  </>
+                }>
+                  <QuestionCircleOutlined style={{ color: "green", width: 50, height: 50 }} />
+                </Tooltip>
 
-    )
-}
+              </>
+            }
+            bordered={true}
+      >
+        <Row>
+          <Col span={8}>
+            <pre suppressContentEditableWarning contentEditable id="textLeft"
+                 style={{
+                   whiteSpace: "pre-wrap",
+                   height: 1000,
+                   borderColor: "black",
+                   borderStyle: "solid",
+                   fontSize: 20
+                 }}></pre>
+          </Col>
+          <Col span={8}>
+            <pre suppressContentEditableWarning contentEditable id="textRight"
+                 style={{
+                   whiteSpace: "pre-wrap",
+                   height: 1000,
+                   borderColor: "black",
+                   borderStyle: "solid",
+                   marginLeft: 10,
+                   fontSize: 20
+                 }}></pre>
+          </Col>
+          <Col span={8}>
+            <pre id="display"
+                 style={{
+                   whiteSpace: "pre-wrap",
+                   fontSize: 20,
+                   marginLeft: 10
+                 }}></pre>
+          </Col>
+        </Row>
+      </Card>
+    </>
 
-render(<Filediff/>, window.document.querySelector('#app-container'));
+  );
+};
+
+render(<Filediff />, window.document.querySelector("#app-container"));
