0032504f81381022ef6c48cee009b93296fcdf34
yanyouqiang <374580618@qq.com>
2022-08-23 09:46:47 +0800

    尝试解决css冲突问题

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -8,18 +8,35 @@ import NewTabRecord from "./modules/NewTabRecord";
 //展示保存标签页
 var showDialog
 
-const modelBind =(show,cancel)=>{
+const modelBind = (show, cancel) => {
     showDialog = show
 }
 
-window.onload = () => {
+//onload 添加事件
+function addLoadEvent(func) {
+    const oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func
+    } else {
+        window.οnlοad = function () {
+            oldonload();
+            func();
+        }
+    }
+
+}
+
+const renderNewRecord = () => {
+    console.log("injected")
     //追加容器
     var dvObj = document.createElement('div');
-    dvObj.id ="app-container"
-    document.body.prepend(dvObj)
+    dvObj.id = "app-container"
+    document.body.append(dvObj)
 
-    render(<NewTabRecord bind={modelBind} />, window.document.querySelector('#app-container'));
+    render(<NewTabRecord bind={modelBind}/>, window.document.querySelector('#app-container'));
 }
+
+addLoadEvent(renderNewRecord)
 
 chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
