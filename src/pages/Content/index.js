7629da2dbfe766ddc1388e9b46c720a2c7f0cb3b
yanyouqiang <374580618@qq.com>
2022-08-23 16:06:49 +0800

    简单化存储

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,50 +1,14 @@
-import {render} from "react-dom";
-import React from "react";
-
-import NewTabRecord from "./modules/NewTabRecord";
-
-
-//展示保存标签页
-var showDialog
-
-const modelBind = (show, cancel) => {
-    showDialog = show
-}
-
-//onload 添加事件
-function addLoadEvent(func) {
-    const oldonload = window.onload;
-    if (typeof window.onload != 'function') {
-        window.onload = func
-    } else {
-        window.οnlοad = function () {
-            oldonload();
-            func();
-        }
-    }
-
-}
-
-const renderNewRecord = () => {
-    console.log("injected")
-    //追加容器
-    var dvObj = document.createElement('div');
-    dvObj.id = "app-container"
-    document.body.append(dvObj)
-
-    render(<NewTabRecord bind={modelBind}/>, window.document.querySelector('#app-container'));
-}
-
-addLoadEvent(renderNewRecord)
+import {newTabRecordTest} from "./modules/newTabRecord";
 
 chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
         if (message.type == 'add-tabs') {
-            showDialog();
+            console.log("chufa")
+            newTabRecordTest();
         }
 
         if (message.type == 'injectEdit') {
-            document.body.contentEditable='true'
+            document.body.contentEditable = 'true'
             alert('inject success')
         }
     }
---
+++ src/pages/Content/modules/newTabRecord.js
@@ -0,0 +1,22 @@
+import {getStorage_record_key, setStorage_record_key} from "../../Function/chromeCommon";
+import randomstring from "rdm-str";
+
+export function newTabRecordTest() {
+
+    getStorage_record_key(records => {
+        const name = prompt("输入要保存标签的名称:")
+        const key = randomstring(7);
+        const record = {
+            key: key,
+            name: name,
+            title: document.title,
+            address: window.location.href,
+            remark: '',
+            tags: ''
+        }
+        records = records ? records : []
+        records.unshift(record)
+        setStorage_record_key(records)
+        alert("保存成功！")
+    })
+}
