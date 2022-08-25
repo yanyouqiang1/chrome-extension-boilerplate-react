df855d2952cd6fd210f26efdf0e90b96e22c3e59
yyq <374580618@qq.com>
2022-08-25 11:04:47 +0800

    fetch

--- src/pages/Function/Usual/myfetch/index.jsx
+++ src/pages/Function/Usual/myfetch/index.jsx
@@ -4,9 +4,8 @@ import "antd/dist/antd.css";
 import { getStorage_fetch, setStorage_fetch } from "../../chromeCommon";
 import { Button, Col, Collapse, message, Modal, Row } from "antd";
 import ReactJson from "react-json-view";
-
 import JSONInput from "react-json-editor-ajrm";
-import './index.css'
+import "./index.css";
 
 
 const { Panel } = Collapse;
@@ -25,70 +24,118 @@ const FetchList = () => {
 
   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    let fetchListElement = fetchList[0];
-    let responsePromise = fetch(fetchListElement.request.url, Object.entries(fetchListElement.request));
-    console.log(responsePromise);
-    setIsModalVisible(false);
-  };
 
   const handleCancel = () => {
     setIsModalVisible(false);
   };
 
 
-  const itemDelete = (key) => {
+  const itemDelete = (item) => {
+    if (!confirm("确认删除？")){
+      return
+    }
     let assign = Object.assign([], fetchList);
 
-    assign = assign.filter(data => data.key != key);
+    assign = assign.filter(data => data.key != item.key);
     setStorage_fetch(assign);
     setFetchList(assign);
 
     message.success("删除成功！", 3);
   };
+
+  const itemSave = (item) => {
+    let newHost = document.querySelector("#url_" + item.key).value;
+    let newName = document.querySelector("#name_" + item.key).value;
+
+    let assign = Object.assign([], fetchList);
+    assign.forEach((it,index)=>{
+      if (it.key==item.key){
+        assign[index].name = newName
+        assign[index].host = newHost
+      }
+    })
+
+    setStorage_fetch(assign);
+    setFetchList(assign);
+
+    message.success("保存成功！", 3);
+
+  }
+  const doTry = (item) => {
+    //替换URL
+    let { url } = item.request;
+    let newHost = document.querySelector("#url_" + item.key).value;
+    url = replaceHost(url, newHost);
+
+
+    //headers
+    const headers = {}
+    item.request.headers.forEach((it,index)=>{
+      let name = it.name.replace(":",'')
+      headers[name] = it.value
+    })
+    //cookies
+
+
+    let responsePromise = fetch(url,{
+      headers: headers,
+      method: item.request.method,
+      body: item.request.body
+    });
+
+    responsePromise.then(response=>response.json()).then(jsonResult=>{
+      console.log(jsonResult);
+      setFetchResult(jsonResult)
+      setIsModalVisible(true)
+    })
+
+  };
+
+  const replaceHost = (url, newHost) => {
+    if (newHost) {
+      let originHost = url.match(/(.*)\/\/(\S*?)\/(.*)/)[2];
+      return url.replace(originHost, newHost);
+    }
+    return url;
+  };
+
+  const nameChange=(item)=>{
+
+  }
   return (
     <>
-      <Modal title="结果展示" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <Modal title="结果展示" visible={isModalVisible} onOk={handleCancel} onCancel={handleCancel}>
         <ReactJson src={fetchResult} />
       </Modal>
 
       <Row>
-        <Col span={24} style={{ textAlign: "right" }}>
-          <Button type="link" onClick={showModal}>
-            结果展示面板
-          </Button>
-        </Col>
         {
           fetchList.map(item =>
             <Col span={24}>
               <Collapse>
                 <Panel header={item.name} key="1" extra={<>
-                  <Button>Try</Button>
+                  <Button onClick={(e) => {itemDelete(item);e.stopPropagation()}} danger type={"link"}>delete</Button>
+                  <Button onClick={(e) => {itemSave(item);e.stopPropagation()}} type={"link"}>save</Button>
+
                 </>}>
-                  <Row>
+                  <Row className="row">
                     <Col span={2} className="left">名称:</Col>
-                    <Col span={20}><input className="inputText" type="text" value={item.name} /></Col>
+                    <Col span={20}><input className="inputText" type="text" value={item.name} id={"name_" + item.key} onChange={()=>nameChange(item)}/></Col>
                   </Row>
-                  <Row>
+                  <Row className="row">
                     <Col span={2} className="left">url:</Col>
-                    <Col span={20}><input className="inputText"  type="text" value={item.request.url} /></Col>
+                    <Col span={20}><input className="inputText" type="text" value={item.host?item.host:''}  id={"url_" + item.key} /></Col>
                   </Row>
-                  <Row>
-                    <Col span={2} className="left">Method:</Col>
-                    <Col span={20}><input className="inputText"  type="text" value={item.request.method} /></Col>
-                  </Row>
-                  <Row>
+                  <Row className="row">
                     <Col span={2} className="left">headers:</Col>
                     <Col span={20}><JSONInput width="100%" placeholder={item.request} id="jsonInput" /></Col>
                   </Row>
                   <Row>
-                    <Col span={2} className="left">result:</Col>
-                    <Col span={20}><ReactJson src={fetchResult}/> </Col>
+                    <Col span={22}>
+                      <Button onClick={()=>doTry(item)} type={"primary"}>Try</Button>
+                    </Col>
                   </Row>
+
                 </Panel>
               </Collapse>
 
