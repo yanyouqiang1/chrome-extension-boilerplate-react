fff647c469539f36bec81857147f9c5f9511b525
yyq <374580618@qq.com>
2024-01-07 21:20:38 +0800

    有问题

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,92 @@
+// 引入store.js
+import store from 'store';
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+// 定义组件
+class Popup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groupName: '',
+      links: [],
+      dataList: [],
+    };
+  }
 
-const Popup = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
-  );
-};
+  handleInputChange = (event) => {
+    this.setState({ groupName: event.target.value });
+  };
+
+  handleRecordClick = () => {
+    const { groupName, links } = this.state;
+    const newData = {
+      groupName,
+      links,
+    };
+    store.set(groupName, newData);
+    const dataList = Object.keys(store.getAll()).map((key) => store.get(key));
+    this.setState({ dataList, groupName: '', links: [] });
+    chrome.tabs.query({}, (tabs) => {
+      tabs.forEach((tab) => {
+        if (!tab.active) {
+          chrome.tabs.remove(tab.id);
+        }
+      });
+    });
+  };
+
+  handleOpenAllClick = (links) => {
+    links.forEach((link) => {
+      chrome.tabs.create({ url: link.url });
+    });
+  };
+
+  handleDeleteClick = (groupName) => {
+    store.remove(groupName);
+    const dataList = Object.keys(store.getAll()).map((key) => store.get(key));
+    this.setState({ dataList });
+  };
+
+  componentDidMount() {
+    const dataList = Object.keys(store.getAll()).map((key) => store.get(key));
+    this.setState({ dataList });
+  }
+
+  render() {
+    const { groupName, links, dataList } = this.state;
+
+    return (
+      <div className="popup-container">
+        <input
+          type="text"
+          value={groupName}
+          onChange={this.handleInputChange}
+          placeholder="输入组名称"
+        />
+        <button onClick={this.handleRecordClick}>记录</button>
+
+        <ul className="record-list">
+          {dataList.map((data, index) => (
+            <li key={index} className="record-item">
+              <span className="group-name">{data.groupName}</span>
+              <button
+                className="open-all-button"
+                onClick={() => this.handleOpenAllClick(data.links)}
+              >
+                打开全部
+              </button>
+              <button
+                className="delete-button"
+                onClick={() => this.handleDeleteClick(data.groupName)}
+              >
+                删除
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Popup;
